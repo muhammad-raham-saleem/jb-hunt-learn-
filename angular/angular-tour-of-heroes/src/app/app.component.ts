0252e615a21bc8d6a00fadeb718fc592b046966e
Muhammad Raham Saleem <muhammadraham.saleem@jbhunt.com>
2025-05-29 11:25:37 -0500

    list now works in heros

--- angular/angular-tour-of-heroes/src/app/app.component.ts
+++ angular/angular-tour-of-heroes/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroesComponent } from "./heroes/heroes.component";
+import { CommonModule } from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, HeroesComponent],
+  imports: [
+  RouterOutlet,
+  HeroesComponent,
+  CommonModule,
+  FormsModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule, NgFor, UpperCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {HEROES} from '../mock-heroes';
+import { HeroDetailComponent } from "../hero-detail/hero-detail.component";
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, NgFor, UpperCasePipe, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero = 'Windstorm'
-
+  heroes: Hero[]=[];
+  constructor(private heroService: HeroService){}
+  selectedHero?:Hero;
+  onSelect(hero: Hero): void{
+    this.selectedHero=hero;
+  }
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
 }
