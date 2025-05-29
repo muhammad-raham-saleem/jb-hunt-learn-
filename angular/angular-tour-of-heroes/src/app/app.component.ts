0b8ef29f9bea6c908cbb13ba36db909ae9a6c394
Muhammad Raham Saleem <muhammadraham.saleem@jbhunt.com>
2025-05-29 09:09:59 -0500

    adding angular tour of heros project

---
+++ angular/angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from "./heroes/heroes.component";
+
+@Component({
+  standalone: true,
+  selector: 'app-root',
+  imports: [RouterOutlet, HeroesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-tour-of-heroes';
+}
